a939623c05fc93c9891085d61f485ab1040f6801
ricardo paulo <ricardodapaulo@gmail.com>
2022-05-16 13:20:18 +0100

    Done1

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+class QuestionsController < ApplicationController
+  def ask; end
+
+  def answer
+    @question = params[:question]
+    @answer = case @question
+              when 'I am going to work'
+                'Great!'
+              when '?'
+                'Silly question, get dressed and go to work!'
+              else
+                "I don't care, get dressed and go to work!"
+              end
+  end
+end
